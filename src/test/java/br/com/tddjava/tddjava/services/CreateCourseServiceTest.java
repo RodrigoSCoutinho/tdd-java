8a6b598beedf6fd0b5a0afa1780fd65ead4201a3
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-30 17:58:37 -0300

    rules

---
+++ src/test/java/br/com/tddjava/tddjava/services/CreateCourseServiceTest.java
@@ -0,0 +1,13 @@
+package br.com.tddjava.tddjava.services;
+
+public class CreateCourseServiceTest {
+    public void should_be_able_to_create_a_new_course(){
+        //Create a new course
+
+        //Create a new service
+
+        //Create a table course (entity)
+
+        //Create a repository of the course
+    }
+}
