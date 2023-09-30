e1259710b19c29a10e969419eacdba3329be9483
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-09-30 19:05:16 -0300

    feat: create entity course

---
+++ src/main/java/br/com/tddjava/tddjava/modules/courses/services/CreateCourseService.java
@@ -0,0 +1,7 @@
+package br.com.tddjava.tddjava.modules.courses.services;
+
+public class CreateCourseService {
+    public void execute(Course course){
+
+    }
+}
--- src/test/java/br/com/tddjava/tddjava/services/CreateCourseServiceTest.java
+++ src/test/java/br/com/tddjava/tddjava/services/CreateCourseServiceTest.java
@@ -1,13 +1,27 @@
 package br.com.tddjava.tddjava.services;
 
+import br.com.tddjava.tddjava.modules.courses.entities.Course;
+import br.com.tddjava.tddjava.modules.courses.services.CreateCourseService;
+import org.junit.Test;
+
 public class CreateCourseServiceTest {
+
+    @Test
     public void should_be_able_to_create_a_new_course(){
         //Create a new course
+        //Create a table course (entity)
+        Course course = new Course();
+        course.setDescription("Course_Description_Test");
+        course.setName("Course_Name");
+        course.setWorkLoad(100);
 
         //Create a new service
-
-        //Create a table course (entity)
+        CreateCourseService createCourseService = new CreateCourseService();
+        Course createdCourse = createCourseService.execute(course);
 
         //Create a repository of the course
+
+        assertNotNull(createdCourse.getId());
+
     }
 }
